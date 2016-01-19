57cae1680b0a9a29a24054d4b031e489ed6ca058
Junior Moura <natmourajr@gmail.com>
2016-01-19 20:35:31 -0200

    Creation of OutputLayer

--- functions/NeuralNetwork/MultiLayerPerceptron.py
+++ functions/NeuralNetwork/MultiLayerPerceptron.py
@@ -81,6 +81,64 @@ class HiddenLayer(object):
         # parameters of the model
         self.params = [self.W, self.b]
 
+class OutputLayer(object):
+    def __init__(self, rng, input, n_in, n_out, W=None, b=None,
+                 activation=T.tanh):
+        """
+        Typical output layer of a MLP: units are fully-connected and have
+        non-linear activation function. Weight matrix W is of shape (n_in,n_out)
+        and the bias vector b is of shape (n_out,).
+
+        NOTE : The nonlinearity used here is tanh
+
+        Output unit activation is given by: tanh(dot(input,W) + b)
+
+        :type rng: numpy.random.RandomState
+        :param rng: a random number generator used to initialize weights
+
+        :type input: theano.tensor.dmatrix
+        :param input: a symbolic tensor of shape (n_examples, n_in)
+
+        :type n_in: int
+        :param n_in: dimensionality of input
+
+        :type n_out: int
+        :param n_out: number of hidden units
+
+        :type activation: theano.Op or function
+        :param activation: Non linearity to be applied in the hidden
+                           layer
+        """
+        self.input = input
+        
+        if W is None:
+            W_values = numpy.asarray(
+                rng.uniform(
+                    low=-numpy.sqrt(6. / (n_in + n_out)),
+                    high=numpy.sqrt(6. / (n_in + n_out)),
+                    size=(n_in, n_out)
+                ),
+                dtype=theano.config.floatX
+            )
+            if activation == theano.tensor.nnet.sigmoid:
+                W_values *= 4
+
+            W = theano.shared(value=W_values, name='W', borrow=True)
+
+        if b is None:
+            b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
+            b = theano.shared(value=b_values, name='b', borrow=True)
+
+        self.W = W
+        self.b = b
+
+        lin_output = T.dot(input, self.W) + self.b
+        self.output = (
+            lin_output if activation is None
+            else activation(lin_output)
+        )
+        # parameters of the model
+        self.params = [self.W, self.b]
 
 
 # start-snippet-2
@@ -139,6 +197,16 @@ class MLP(object):
             n_in=n_hidden,
             n_out=n_out
         )
+        
+        
+        self.outputLayer = OutputLayer(
+            rng=rng,
+            input=self.hiddenLayer.output,
+            n_in=n_in,
+            n_out=n_hidden,
+            activation=T.tanh
+        )
+        
         # end-snippet-2 start-snippet-3
         # L1 norm ; one regularization option is to enforce L1 norm to
         # be small
---
+++ tutorials/init_mlp.py
@@ -0,0 +1,4 @@
+from NeuralNetwork import MultiLayerPerceptron as mlpLib
+
+print 'Creating MLP object'
+mlp = mlpLib.CreateMLP (28*28,500,10)
